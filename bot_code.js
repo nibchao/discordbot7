6873b35bce655cd17c7126cbd95f18d77a245993
nibchao <nibchao@gmail.com>
2022-03-25 14:27:25 -0700

    uninstalled youtube-api, installed google-api https://developers.google.com/youtube/v3/quickstart/nodejs

--- bot_code.js
+++ bot_code.js
@@ -4,7 +4,8 @@ const { Client, Intents, Message, Permissions, GuildEmojiRoleManager, MessageEmb
 const { token, clientID, guildID } = require("./credentials/discordTestingBotCredentials.json"); // comment this when uploading to main bot
 
 // https://www.npmjs.com/package/youtube-api
- const YoutubeApi = require('youtube-api');
+ const google = require('googleapis');
+ const OAuth2 = google.auth.OAuth2;
  const youtubeCredentials = require(`./credentials/youtubeCredentials.json`);
 
 // Check for live streams
@@ -159,14 +160,17 @@ client.once("ready", () =>
 
     // hardcoded youtube channel url at the moment; if it's possible, try to get youtube channel url by fetching via search parameters https://developers.google.com/youtube/v3/docs/search/list?apix=true
     // https://stackoverflow.com/a/1431113
+
+    // https://developers.google.com/youtube/v3/quickstart/nodejs
+
     let ytChannelURL = 'UCSD0MKMFT0bZP4jj6c5ihMw'; // need to see what happens when user has custom channel URL
     console.log(ytChannelURL);
     let latestVideoURL = '';
     latestVideoURL = ytChannelURL.replaceAt(1, 'U'); // replacing the C with a U makes the playlist request work for some reason
     console.log(latestVideoURL);
 
-
-    console.log(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${latestVideoURL}&maxResults=1&key=${youtubeCredentials.api_key}`);
+    let googleURLTemp = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${latestVideoURL}&maxResults=1&key=${youtubeCredentials.api_key}`;
+    //console.log(googleURLTemp.items.snippet.resourceId.videoId); // need to somehow access the contents in the URL instead of accessing the actual URL
     // then get videoID from above somehow and use in `https://www.youtube.com/watch?v=${videoID}` to post for video URL in notification message
 
     //startBot();
